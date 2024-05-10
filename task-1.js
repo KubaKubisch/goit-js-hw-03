9d971fa40a7f778822e5a915c58a8d66425a32a6
KubaKubisch <156842612+KubaKubisch@users.noreply.github.com>
2024-05-10 16:04:43 +0200

    1

--- task-1.js
+++ task-1.js
@@ -8,7 +8,7 @@ function makeTransaction(quanity, pricePerDroid, customerCredits) {
    } else return `Insufficient funds!`
 }
 
-console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(5, 3000, 23000)); 
 console.log(makeTransaction(3, 1000, 15000)); 
 console.log(makeTransaction(10, 5000, 8000)); 
 console.log(makeTransaction(8, 2000, 10000)); 
