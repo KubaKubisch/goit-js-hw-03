214b9d77602622f7c585e478aad8a9bdde194761
KubaKubisch <156842612+KubaKubisch@users.noreply.github.com>
2024-05-11 17:36:39 +0200

    ready

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,20 @@
-function makeArray(firstArray, secondArray, maxLength) {
-    const result = firstArray.concat(secondArray);
-    let finish = result.slice(0, maxLength);
-    return finish
+
+
+function filterArray(numbers, value) {
+    let readyArray = []
+for (let i=0; i < numbers.length; i += 1) {
+if (numbers[i] > value){
+    readyArray.push(numbers[i])
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+}
+return readyArray;
+}
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
